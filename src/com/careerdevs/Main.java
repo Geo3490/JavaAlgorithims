79815977c48ec8b42bfc93ceb2e67387429710fd
Geo3490 <geo40127@gmail.com>
2021-12-21 12:41:54 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,73 @@
+package com.careerdevs;
+
+        //1
+public class Challenge{
+    public static int getFirstValue (int[] arr){
+        return arr[0];
+    }
+}
+        //2
+
+public class Challenge{
+    public static int addition(int[] num){
+        return ++num;
+    }
+}
+        //3
+
+public class Challenge {
+    public static int search(int arr[], int item) {
+
+    }
+}
+
+        //4
+
+public class Challenge {
+            public static int convert(int minutes) {
+
+            }
+        }
+
+        //5
+
+ public class Program {
+    public static String giveMeSomething(String a) {
+
+            }
+        }
+
+        //6
+
+public class Challenge {
+    public static boolean reverse(boolean b) {
+
+    }
+}
+
+        //7
+
+public class Program {
+    public static int countWords(String s) {
+
+    }
+}
+
+        //8
+
+public class ExistsANumberHigher {
+    public static boolean existsHigher(int[] arr, int n) {
+
+    }
+}
+
+        //9
+
+public class Vowels {
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        // Your code here
+
+        return vowelsCount;
+    }
+}
