a13278cc20b0dd8e4ae24eedda762ffd7cb3ecc0
Geo3490 <geo40127@gmail.com>
2021-12-21 15:17:55 -0500

    updated

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,19 +1,27 @@
 package com.careerdevs;
 
         //1
+//Create a function that takes an array containing only numbers and return the first element.
+
 public class Challenge{
     public static int getFirstValue (int[] arr){
         return arr[0];
     }
 }
+
         //2
+//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
 
 public class Challenge{
     public static int addition(int[] num){
         return ++num;
     }
 }
+
         //3
+//Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
+
 
 public class Challenge {
     public static int search(int arr[], int item) {
@@ -22,22 +30,29 @@ public class Challenge {
 }
 
         //4
+//Write a function that takes an integer minutes and converts it to seconds.
+//
+//Examples
+
 
 public class Challenge {
             public static int convert(int minutes) {
 
-            }
-        }
+      }
+}
 
         //5
+//Write a function that returns the string "something" joined with a space " " and the given argument a.
+
 
  public class Program {
     public static String giveMeSomething(String a) {
 
-            }
-        }
+    }
+}
 
         //6
+//Create a function that reverses a boolean value.
 
 public class Challenge {
     public static boolean reverse(boolean b) {
@@ -46,6 +61,7 @@ public class Challenge {
 }
 
         //7
+//Create a method that takes a string and returns the word count. The string will be a sentence.
 
 public class Program {
     public static int countWords(String s) {
@@ -54,6 +70,7 @@ public class Program {
 }
 
         //8
+//Write a function that returns true if there exists at least one number that is larger than or equal to n.
 
 public class ExistsANumberHigher {
     public static boolean existsHigher(int[] arr, int n) {
@@ -62,6 +79,7 @@ public class ExistsANumberHigher {
 }
 
         //9
+//Create a function that takes a string and returns the number (count) of vowels contained within it.
 
 public class Vowels {
     public static int getCount(String str) {
